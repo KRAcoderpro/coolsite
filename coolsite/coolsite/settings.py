b32dcf2630ee40472142812c166813a56579cae8
Rubicscube <kharubnfc73@gmail.com>
2022-10-07 19:33:54 +0300

    added Exceptions

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2nyw=n8-=kc4r^s+w++q!sf%coajw@2%!fge#%aslg@(tot^(q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
